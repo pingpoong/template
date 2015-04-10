344d647eeccd94318c0d5b73d1d1b798b68a7d51
Thoi Tran Duyen Hai <hai.thoi@sutrixmedia.com>
2015-04-10 13:19:56 +0700

    Create animation effects

---
+++ source/assets/js/plugins/plugin.js
@@ -0,0 +1,69 @@
+/**
+ *  @name plugin
+ *  @description description
+ *  @version 1.0
+ *  @options
+ *    option
+ *  @events
+ *    event
+ *  @methods
+ *    init
+ *    publicMethod
+ *    destroy
+ */
+;(function($, window, undefined) {
+  var pluginName = 'plugin';
+  var privateVar = null;
+  var privateMethod = function() {
+    // to do
+  };
+
+  function Plugin(element, options) {
+    this.element = $(element);
+    this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data(), options);
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function() {
+      var that = this;
+      // initialize
+      // add events
+    },
+    publicMethod: function(params) {
+      // to do
+      this.element.trigger('customEvent');
+    },
+    destroy: function() {
+      // deinitialize
+      $.removeData(this.element[0], pluginName);
+    }
+  };
+
+  $.fn[pluginName] = function(options, params) {
+    return this.each(function() {
+      var instance = $.data(this, pluginName);
+      if (!instance) {
+        $.data(this, pluginName, new Plugin(this, options));
+      } else if (instance[options]) {
+        instance[options](params);
+      } else {
+        window.console && console.log(options ? options + ' method is not exists in ' + pluginName : pluginName + ' plugin has been initialized');
+      }
+    });
+  };
+
+  $.fn[pluginName].defaults = {
+    key: 'value'
+  };
+
+  $(function() {
+    $('[data-' + pluginName + ']')[pluginName]({
+      key: 'custom'
+    });
+    $('[data-' + pluginName + ']').on('customEvent', function(){
+      // to do
+    });
+  });
+
+}(jQuery, window));
